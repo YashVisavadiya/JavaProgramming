1a2765a749b66e70702f965e7186d050ffa60563
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2022-03-03 20:30:25 +0530

    commit

---
+++ Highest2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Highest2{
+	public static void main(String args[]){
+
+		Scanner sc= new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int max = a > b ? ( a > c ? a : c ) : ( b > c ? b : c );
+
+		System.out.println(max+" is MAX"); 
+	}
+}
