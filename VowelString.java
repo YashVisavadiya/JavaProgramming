08b0b0781043820c3d5e05f95a3b9e00da9f562d
YashVisavadiya <90129643+YashVisavadiya@users.noreply.github.com>
2022-04-21 01:41:12 +0530

    commit

---
+++ VowelString.java
@@ -0,0 +1,19 @@
+package com.college;
+
+import java.util.Scanner;
+
+public class VowelString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s;
+        System.out.println("Enter a String : ");
+        s=sc.nextLine();
+
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'||s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+                System.out.print(s.charAt(i));
+            }
+        }
+    }
+}
